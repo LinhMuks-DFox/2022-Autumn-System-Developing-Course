7b2469c0bbfc89c2569b02359d6c99b23ef4bce3
LinhMux <muxxum65535@gmail.com>
2023-01-06 22:09:23 +0900

    Adding Dependency

---
+++ Final/mklearn/convolution/ConvoNTime.py
@@ -0,0 +1,9 @@
+__all__ = [
+    "ConvNDimNTimes",
+    "ConvTransposeNDimNTimes",
+    "create_symmetries_conv_transpose",
+]
+
+from ._ConvNDimNTimes import ConvNDimNTimes
+from ._ConvTransposeNDimNTimes import ConvTransposeNDimNTimes
+from ._functional import create_symmetries_conv_transpose
