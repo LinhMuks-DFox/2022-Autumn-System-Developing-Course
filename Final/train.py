6f279896f59985db9329875c5ffd170f714e2717
LinhMux <muxxum65535@gmail.com>
2023-01-07 17:50:45 +0900

    add plot chunk

--- Final/train.py
+++ Final/train.py
@@ -1,9 +1,11 @@
+import random
 from typing import Tuple, Any
 
 import numpy as np
 import torch.utils.data
 import torchvision.transforms
 from torchvision.datasets import MNIST
+import matplotlib.pyplot as plt
 
 from ImageAutoEncoder.ImageAutoEncoder import *
 from mklearn.model_train.model_training import *
@@ -33,16 +35,29 @@ alchemy_furnace = AlchemyFurnace(parameter := AlchemyParameters(
     model_name="ImageAutoEncoder",
     optimizer=torch.optim.Adam(model.parameters()),
     loss_function=torch.nn.MSELoss(),
-    epochs=3,
+    epochs=30,
     device="cuda:0",
     train_set=demo_dataset,
     train_kernel=train_kernel,
     train_data_loader=train_dataloader,
     test_data_loader=test_loader,
-    test_in_train=True,
+    test_in_train=False,
     test_kernel=train_kernel,
-    verbose=True
+    verbose=True,
+    validation_loader=test_loader
 ))
 alchemy_furnace.train()
+alchemy_furnace.score()
 alchemy_furnace.plot_loss()
-# print(parameter)
+alchemy_furnace.save()
+print("Overall, test loss: ", alchemy_furnace.test_loss_)
+# plots
+fig, axes = plt.subplots(2, 10, figsize=(10, 4))
+for i in range(10):
+    r = random.randint(100, 1000)
+    org_data = demo_dataset[i + r][0].unsqueeze(0)
+    encoded = model.encoder_(org_data)
+    decoded = model.decoder_(encoded)
+    axes[0, i].imshow(encoded.detach().cpu().numpy()[0, 0, :, :])
+    axes[1, i].imshow(decoded.detach().cpu().numpy()[0, 0, :, :])
+plt.show()
